e70a9e5a778f5d2a8ce582d9f69706e0a124d9a9
ClairKyu <jkchoi3071@gmail.com>
2023-07-31 11:23:38 +0900

    AWS RDS와 연동 & dotenv로 환경변수 등록

--- app/src/models/User.js
+++ app/src/models/User.js
@@ -10,16 +10,21 @@ class User {
 
   async login() {
     const client = this.body;
-    const { id, pw } = await UserStorage.getUSerInfo(client.id); //클라이언트가 입력한 ID값 스토리지에 메소드로 전달
-    if (id) {
-      if (id === client.id && pw === client.pw) {
-        return { success: true };
+    try {
+      //클라이언트가 입력한 ID값 스토리지에 메소드로 전달
+      //promise 객체를 받아올때까지 기다려! 라는 뜻
+      const user = await UserStorage.getUSerInfo(client.id);
+      if (user) {
+        if (user.id === client.id && user.pw === client.pw) {
+          return { success: true };
+        }
+        return { success: false, msg: "비밀번호가 틀렸습니다." };
       }
-      return { success: false, msg: "비밀번호가 틀렸습니다." };
+      return { success: false, msg: "존재하지 않는 아이디 입니다." };
+    } catch (err) {
+      return { success: false, msg: err };
     }
-    return { success: false, msg: "존재하지 않는 아이디 입니다." };
   }
-
   async register() {
     const client = this.body;
     try {
--- app/src/models/UserStorage.js
+++ app/src/models/UserStorage.js
@@ -1,68 +1,27 @@
 "use strict";
 
-const { URLSearchParams } = require("url");
-
-const fs = require("fs").promises; //파일 시스템, 파일 읽을 수 있도록
+const db = require("../config/db");
 
 class UserStorage {
-  static #getUserInfo(data, id) {
-    //변수나 메서드는 클래스 최 상단
-    const users = JSON.parse(data);
-    const idx = users.id.indexOf(id);
-    const usersKeys = Object.keys(users); // => [id, pw, name]
-    const userInfo = usersKeys.reduce((newUser, info) => {
-      newUser[info] = users[info][idx];
-      return newUser;
-    }, {});
-
-    return userInfo;
-  }
-
-  static #getUsers(data, isAll, fields) {
-    const users = JSON.parse(data);
-    if (isAll) return users;
-    const newUsers = fields.reduce((newUsers, field) => {
-      if (users.hasOwnProperty(field)) {
-        //users에 해당하는 키값이 있는지 물어보는 것
-        newUsers[field] = users[field];
-      }
-      return newUsers;
-    }, {});
-    return newUsers;
-  }
-
-  static getUsers(isAll, ...fields) {
-    return fs
-      .readFile("./src/databases/users.json")
-      .then((data) => {
-        return this.#getUsers(data, isAll, fields); //은닉화된 메서드
-      })
-      .catch(console.error);
-    //은닉화 후 메소드를 추가해서 전달해줘야 한다.
-    //const users = this.#users;
-  }
-
   static getUSerInfo(id) {
-    return fs
-      .readFile("./src/databases/users.json")
-      .then((data) => {
-        return this.#getUserInfo(data, id); //은닉화된 메서드
-      })
-      .catch(console.error);
+    //promise 시간이 오래걸리는 구문을 실행시킬 때 사용
+    return new Promise((resolve, reject) => {
+      const query = "SELECT * FROM users WHERE id = ?;";
+      db.query(query, [id], (err, data) => {
+        if (err) reject(err); //실패했을 때
+        resolve(data[0]); //성공했을 때 데이터 전달
+      });
+    });
   }
 
   static async save(userInfo) {
-    const users = await this.getUsers(true); //모든 필드명 가져오고싶을때 true
-    if (users.id.includes(userInfo.id)) {
-      //클라이언트에 입력한 데이터가 데이터베이스에 있는지 확인
-      throw "이미 존재하는 아이디 입니다"; //error 던져주기
-    }
-    users.id.push(userInfo.id);
-    users.name.push(userInfo.name);
-    users.pw.push(userInfo.pw);
-    //데이터 추가
-    fs.writeFile("./src/databases/users.json", JSON.stringify(users));
-    return { success: true }; //위에 문장은 아무것도 반환해주지 않으니까 트루값 반환
+    return new Promise((resolve, reject) => {
+      const query = "INSERT INTO users(id, name, pw) VALUES(?, ?, ?);";
+      db.query(query, [userInfo.id, userInfo.name, userInfo.pw], (err) => {
+        if (err) reject(`${err}`); //실패했을 때
+        resolve({ success: true }); //성공했을 때 데이터 전달
+      });
+    });
   }
 }
 
